c7c9a421c7cae0c3c9aef049b90dea807fedbf4e
HyperCops <anthonydealmeidamajor@gmail.com>
2026-04-01 17:14:15 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,1 @@
-variable "render_api_key" {
-  type      = string
-  sensitive = true
-}
 
-variable "render_owner_id" {
-  type = string
-}
-
-variable "image_url" {
-  type = string
-}
-
-variable "image_tag" {
-  type = string
-}
